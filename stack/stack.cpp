a1eb7b910b9b84eaf45bf1a71984c67cf3481767
Emma <emmanuelponciano@gmail.com>
2023-07-30 12:02:23 -0400

    Add pop and push methods

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -29,6 +29,28 @@ public:
         height = 1;
     }
 
+    void push(int value)
+    {
+        Node *newNode = new Node(value);
+        newNode->next = top;
+        top = newNode;
+        height++;
+    }
+
+    int pop()
+    {
+        if (height == 0)
+            return INT16_MIN;
+
+        Node *temp = top;
+        int poppedValue = top->value;
+        top = top->next;
+        delete temp;
+        height--;
+
+        return poppedValue;
+    }
+
     void printStack()
     {
         Node *temp = top;
@@ -52,9 +74,13 @@ public:
 
 int main()
 {
-    Stack *stack = new Stack(4);
+    Stack *stack = new Stack(1);
 
     stack->getTop();
     stack->getHeight();
+    stack->push(2);
+    stack->push(3);
+    stack->printStack();
+    stack->pop();
     stack->printStack();
 }
