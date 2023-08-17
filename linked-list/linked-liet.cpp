c8b9def9c46fb4c51fc85cd89f46f12d9f230acc
Emma <emmanuelponciano@gmail.com>
2023-08-17 16:20:32 -0400

    add linked list ctor

---
+++ linked-list/linked-liet.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        next = nullptr;
+    }
+};
+
+class LinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+    int length;
+
+public:
+    LinkedList(int value)
+    {
+        Node *newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    void PrintList()
+    {
+        Node *temp = head;
+
+        while (temp)
+        {
+            cout << temp->value << endl;
+            temp = temp->next;
+        }
+    }
+
+    void getHead()
+    {
+        cout << "Head: " << head->value << endl;
+    }
+
+    void getTail()
+    {
+        cout << "Tail: " << tail->value << endl;
+    }
+
+    void getLength()
+    {
+        cout << "Length: " << length << endl;
+    }
+};
+
+int main()
+{
+    LinkedList *linkedList = new LinkedList(8);
+
+    linkedList->getHead();
+    linkedList->getTail();
+    linkedList->getLength();
+
+    linkedList->PrintList();
+}
