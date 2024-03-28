aeff3fa26878a1248494f7bc3840435787720849
Emma <emmanuelponciano@gmail.com>
2024-03-27 22:49:20 -0400

    fix

---
+++ stack/stack.cpp
@@ -0,0 +1,26 @@
+class Node
+{
+public:
+    int value;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        next = nullptr;
+    }
+};
+
+class Stack {
+private:
+    Node *top;
+    int height;
+
+public:
+    Stack(int value)
+    {
+        Node *newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+};
